ef3027a691ed848230972eb3753a704d6a7046d2
lkusters <lienekekusters@gmail.com>
2016-12-29 12:13:47 +0100

    Settings for tip of specified speed
    Can set background color and time per cycle and width of tip.

--- ledclock/ledclock.ino
+++ ledclock/ledclock.ino
@@ -26,13 +26,13 @@ const uint16_t ledCount = 167;
 // Set the background color { red, green, blue} max values are 255
 const uint8_t bckColor[3]  = { 255, 0, 0 };
 // Set the color of the tip { red, green, blue} max values are 255
-//const uint8_t tipColor[3]  = { 0, 0, 255 };
+const uint8_t tipColor[3]  = { 0, 0, 255 };
 // Set the brightness to use (the maximum is 31).
-
-const uint8_t brightness = 5;
-// Set the colors for the strip { red, green, blue} max values are 255
-const uint8_t numColors = 3; // Current setting is three different colors
-const uint8_t tipColors[numColors][3]  = {{ 0, 0, 255 } , { 255, 0, 0 } , { 0, 255, 0 } };
+const uint8_t brightness = 3;
+// Set the number of leds that make the tip
+const uint8_t tipWidth = 5;
+// Set the number of seconds that should take a single roundtrip of the tip
+const uint8_t cycleTime = 10;
 
 
 /*
@@ -41,37 +41,29 @@ const uint8_t tipColors[numColors][3]  = {{ 0, 0, 255 } , { 255, 0, 0 } , { 0, 2
 // Create a buffer for holding the colors (3 bytes per color).
 rgb_color colors[ledCount];
 uint16_t stepCount;
-uint16_t colorCount;
+const uint16_t dtime = cycleTime*1000 / ledCount;
 
 void setup() {
   // put your setup code here, to run once:
-  uint8_t* tipColor = tipColors[0]; // initialize with first color
-  initstrip(tipColor);
+  initstrip(bckColor);
   
   ledStrip.write(colors, ledCount, brightness);
   stepCount = 0;
-  colorCount = 0;
 }
 
 void loop() {
   
   // put your main code here, to run repeatedly:
   stepCount = stepCount % ledCount;
-  if (stepCount == 0)
-  {
-    colorCount += 1;
-  }
-  colorCount = colorCount % numColors;
-  uint8_t* tipColor = tipColors[colorCount];
 
+  initstrip(bckColor);
   // Update color
-  colors[stepCount].red = tipColor[0];
-  colors[stepCount].green = tipColor[1];
-  colors[stepCount].blue = tipColor[2];
+  colortip(tipColor, stepCount);
+  
   ledStrip.write(colors, ledCount, brightness);
 
   stepCount += 1;
-  delay(40);
+  delay(dtime);
 }
 
 void initstrip(uint8_t color[3]){
@@ -83,6 +75,20 @@ void initstrip(uint8_t color[3]){
     colors[i].red = color[0];
     colors[i].green = color[1];
     colors[i].blue = color[2];
+  }
+}
+
+void colortip(uint8_t color[3], uint8_t firstIndex){
+/* 
+ *  fill tip with predefined color, firstIndex gives index of tip
+ */
+ uint16_t index;
+ for(uint16_t i = 0; i < tipWidth; i++)
+  {
+    index = (firstIndex-i+ledCount) % ledCount;
+    colors[index].red = color[0];
+    colors[index].green = color[1];
+    colors[index].blue = color[2];
   }
 }
 
